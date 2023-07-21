2ed300413b959c024327eedd1d5dd3c17771e398
Shivam kumar singh <shivamkrsingh4688@gmail.com>
2023-07-21 06:15:04 +0530

    build add

---
+++ client/build/precache-manifest.40c8175a15557ea1f4922ae67a973363.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "836d876ec9202d7b60c5523e38e098d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c6481a2c0f9968c0f36",
+    "url": "/static/css/main.f7377c97.chunk.css"
+  },
+  {
+    "revision": "20ae1808e1c9af56956c",
+    "url": "/static/js/2.bfb52602.chunk.js"
+  },
+  {
+    "revision": "90377550dd86ed303dd30d8f487872de",
+    "url": "/static/js/2.bfb52602.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c6481a2c0f9968c0f36",
+    "url": "/static/js/main.3826ecb6.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "5c0c63fb62ec323a10598d7e9de4609e",
+    "url": "/static/media/memories.5c0c63fb.png"
+  }
+]);
